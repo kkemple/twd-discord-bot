ae798facdc41f0e3cedc52106bd292b9f4cc44b9
Kurt Kemple <kurtiskemple@gmail.com>
2020-08-20 20:47:18 -0400

    update bot

--- src/index.ts
+++ src/index.ts
@@ -2,58 +2,52 @@ import './env';
 
 import Discord from 'discord.js';
 const client = new Discord.Client({
-  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  partials: ['MESSAGE', 'REACTION'],
+});
+const guild = new Discord.Guild(client, { id: process.env.SERVER_ID });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client?.user?.tag}!`);
 });
 
-async function main() {
-  try {
-    const guild = new Discord.Guild(client, { id: '746058055824703629 ' });
-
-    client.on('ready', () => {
-      console.log(`Logged in as ${client?.user?.tag}!`);
-    });
-
-    client.on('message', message => {
-      if (message.content === '!ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
-      }
-    });
-
-    client.on('messageReactionAdd', async (reaction, user) => {
-      if (
-        reaction.message.id !== process.env.MESSAGE_ID ||
-        reaction.emoji.name !== '☑️'
-      ) {
-        return;
-      }
-
-      const member = await guild.members.fetch(user.id);
-      const role = new Discord.Role(
-        client,
-        { id: '746076573685252186' },
-        guild
-      );
-
-      member.roles.set([role]);
-    });
-
-    client.on('messageReactionRemove', async (reaction, user) => {
-      if (
-        reaction.message.id !== process.env.MESSAGE_ID ||
-        reaction.emoji.name !== '☑️'
-      ) {
-        return;
-      }
-      const member = await guild.members.fetch(user.id);
-      member.roles.set([]);
-    });
-
-    client.login(process.env.BOT_TOKEN);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
+client.on('message', message => {
+  if (message.content.toLowerCase() === '!ping') {
+    message.channel.send('Pong.');
   }
-}
+});
 
-main();
+client.on('messageReactionAdd', async (reaction, user) => {
+  try {
+    if (
+      reaction.message.id !== process.env.MESSAGE_ID ||
+      reaction.emoji.name !== '☑️'
+    ) {
+      return;
+    }
+
+    const member = await guild.members.fetch(user.id);
+    const role = new Discord.Role(client, { id: process.env.ROLE_ID }, guild);
+
+    member.roles.set([role]);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+  try {
+    if (
+      reaction.message.id !== process.env.MESSAGE_ID ||
+      reaction.emoji.name !== '☑️'
+    ) {
+      return;
+    }
+
+    const member = await guild.members.fetch(user.id);
+    member.roles.set([]);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
